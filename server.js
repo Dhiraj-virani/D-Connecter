72f45639e36bd9c34a88979f7592940ad0c777fb
dhiraj-virani <dhiraj_virani16@yahoo.com>
2022-03-21 08:54:21 -0400

    Profile & User model

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+const { application } = require('express');
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Api Running'));
+//connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false}));
+
+app.get('/', (req,res) => res.send('Api Running again'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 
 const PORT = process.env.PORT || 5000;
 
